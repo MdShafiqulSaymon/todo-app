ccbd0bdcb2017a5e9d48a6f46a2978e2d3631029
Md. Shafiqul Hasan <mdshafiqulsaymon@gmail.com>
2025-05-22 23:10:54 +0600

    Added CORS Policy

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors({
-    origin: '*',
+  
+  // Enable CORS for all origins
+  app.enableCors({
+    origin: true, // Allows all origins
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
     credentials: true,
+  });
+
+  // Enable validation globally
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
   }));
+
   await app.listen(3000);
+  console.log('Application is running on: http://localhost:3000');
 }
 bootstrap();
